033e370459afa5f5b2490e2012f2921fae4c1ca5
Maciek Stasieluk <macrusher@gmail.com>
2015-10-23 01:22:58 +0200

    version bump

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'vazco:ci-test',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'Ignore this package it does nothing',
   // URL to the Git repository containing the source code for this package.
